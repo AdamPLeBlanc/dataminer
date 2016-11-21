82690dd966f80b8aef1e9e9d65355252158d9d39
LeBlanc <adam.p.leblanc@boeing.com>
2016-11-21 10:21:24 -0800

    adding main script

---
+++ minedata.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import struct
+
+'''
+possible args to pass in
+-filepath
+-range of bytes to process from data
+-logic string to validate data with
+-endianess to test
+-flag for bitshiftmode
+
+data = file.readall
+
+if bitshiftmode
+ 
+
+
+
+'''
